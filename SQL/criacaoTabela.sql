eae1eb67c4e96efce23bd78e56306b966ad8d21f
Tiago DAgostino <djhamo@gmail.com>
2021-03-08 18:56:53 -0300

    Criação da tabela de Paises. Modelo de Pais json

--- SQL/criacaoTabela.sql
+++ SQL/criacaoTabela.sql
@@ -9,12 +9,30 @@
     "Active": 0,
     "Date": "2020-01-22T00:00:00Z",
 */
+/*
+[
+    {
+        "Country": "Mexico",
+        "Slug": "mexico",
+        "ISO2": "MX"
+    },
+*/
+CREATE TABLE PAIS
+(
+	ID				int 			not null auto_increment, #	pk
+	NOME			varchar(50)     not null,
+	SLUG			varchar(50)     null,
+	SIGLA           varchar(2)      null,
+	CONSTRAINT PK_PAIS PRIMARY KEY (ID)
+);
 
+/*
+[{"Country":"Brazil","CountryCode":"BR","Province":"","City":"","CityCode":"","Lat":"-14.24","Lon":"-51.93","Cases":0,"Status":"confirmed","Date":"2020-01-22T00:00:00Z"},
+*/
 create table dados_paises
 (
 	id				int 			not null auto_increment, #	pk
-	country			varchar(40)		not null,
-	countryCode		char(2)			not null,
+    id_pais         int				not null,
 	lat				decimal			not null,
 	lon				decimal 		not null,
 	confirmed		int				null,
@@ -23,5 +41,7 @@ create table dados_paises
 	active			int				null,
 	date			datetime		not null,
 	
-	constraint 		pk_id			primary key (id)
+	constraint 		pk_id			primary key (id),
+	CONSTRAINT FK_DADOS_PAISES_PAIS FOREIGN KEY (ID_PAIS)
+		REFERENCES PAIS (ID)
 );
