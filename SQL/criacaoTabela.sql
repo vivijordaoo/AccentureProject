76898f63428b03c85342d6fa6da3eeac479c2a5c
Leonardo Moreno <lmorenogr@gmail.com>
2021-03-09 11:18:03 -0300

    formatação no script de tabelas

--- SQL/criacaoTabela.sql
+++ SQL/criacaoTabela.sql
@@ -17,32 +17,42 @@
         "ISO2": "MX"
     },
 */
-CREATE TABLE PAIS
+create table pais
 (
-	ID				int 			not null auto_increment, #	pk
-	NOME			varchar(50)     not null,
-	SLUG			varchar(50)     null,
-	SIGLA           varchar(2)      null,
-	CONSTRAINT PK_PAIS PRIMARY KEY (ID)
+	id				int 			not null auto_increment, #	pk
+	nome			varchar(50)     not null,
+	slug			varchar(50)     null,
+	sigla           varchar(2)      null,
+	CONSTRAINT 		pk_pais 		primary key (id)
 );
 
 /*
-[{"Country":"Brazil","CountryCode":"BR","Province":"","City":"","CityCode":"","Lat":"-14.24","Lon":"-51.93","Cases":0,"Status":"confirmed","Date":"2020-01-22T00:00:00Z"},
+[{"Country":"Brazil","CountryCode":"BR",
+"Province":"","City":"","CityCode":"",
+"Lat":"-14.24","Lon":"-51.93",
+"Cases":0,"Status":"confirmed","Date":"2020-01-22T00:00:00Z"},
 */
 create table dados_paises
 (
-	id			int 			not null auto_increment, #	pk
-	country			varchar(40)		not null,
-	countryCode		char(2)			not null,
-	lat			decimal			not null,
-	lon			decimal 		not null,
-	confirmed		int			null,
-	deaths			int			null,
-	recovered		int			null,
-	active			int			null,
+	id				int 			not null auto_increment, #	pk
+	id_pais			int				not null,	 			 #  fk pais
+	lat				decimal			not null,
+	lon				decimal 		not null,
+	confirmed		int				null,
+	deaths			int				null,
+	recovered		int				null,
+	active			int				null,
 	date			datetime		not null,
 	
 	constraint 		pk_id			primary key (id),
-	CONSTRAINT FK_DADOS_PAISES_PAIS FOREIGN KEY (ID_PAIS)
-		REFERENCES PAIS (ID)
+	constraint fk_dados_paises_pais foreign key (id_pais)
+		references pais(id)
+);
+
+create table log 
+(
+	id			int			not null auto_increment, #pk
+	data		datetime	not null,
+	descricao	text		not null,
+	constraint 	pk_log		primary key (id)
 );
